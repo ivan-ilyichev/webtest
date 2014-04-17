733a98245cdd14dde71675cbd42478f1e8497ad4
Ivan <ilyichev.i@gmail.com>
2014-04-17 16:30:22 +0400

    SirnalR experiments

---
+++ MyStore/app/MyStore.Web/SignalR.Sample/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Microsoft.AspNet.SignalR.StockTicker.Startup), "ConfigureSignalR")]
+
+namespace Microsoft.AspNet.SignalR.StockTicker
+{
+    public static class Startup
+    {
+        public static void ConfigureSignalR(IAppBuilder app)
+        {
+            // For more information on how to configure your application using OWIN startup, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+             app.MapSignalR();
+        }
+    }
+}
